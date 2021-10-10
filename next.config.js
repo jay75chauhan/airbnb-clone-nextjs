2456141efdc0da5ab348c316765c0c130691b62b
jay75chauhan <chauhan75jayk@gmail.com>
2021-10-10 16:20:20 +0530

    pwa add

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
   images: {
     domains: [
       "links.papareact.com",
@@ -15,4 +23,4 @@ module.exports = {
     mapbox_key:
       "pk.eyJ1IjoiamF5NzVjaGF1aGFuIiwiYSI6ImNrczFqZjduYTA0NDMydnBmandxZThjNjUifQ.9siLndUHBjD12AuIcbc_fA",
   },
-};
+});
