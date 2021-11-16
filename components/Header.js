496a62342c4137a56487990448638bffcc8d51d7
jay75chauhan <chauhan75jayk@gmail.com>
2021-11-16 21:45:50 +0000

    adding yarn

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRangePicker } from "react-date-range";
 import { useRouter } from "next/dist/client/router";
-import { route } from "next/dist/next-server/server/router";
+import { route } from "next/router";
 
 function Header({ placeholder }) {
   const [session] = useSession();
